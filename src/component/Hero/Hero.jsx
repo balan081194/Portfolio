b56d16e09b3c66a1b28c9ca51ace877ceba78cf7
balan081194 <123051259+balan081194@users.noreply.github.com>
2024-12-03 11:39:56 +0530

    update

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -8,7 +8,7 @@ export const Hero = () => {
    <section className={styles.container}>
     <div className={styles.content}>
         <h2 className={styles.title}>Hi, I'm Balan</h2>
-        <p className={styles.description}>I'm a Full Stack Developer with one year of experience using
+        <p className={styles.description}>I'm a Full Stack Developer with two year of experience using
             Reactjs, Nodejs and MongoDB. Reach out if you'd like to more learn!
         </p>
         <a href={Resume} download className={styles.contactBtn}>Download CV</a>
